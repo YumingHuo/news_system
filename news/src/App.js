74eb1636b6344109aa912283da335cd6f6c4f685
YumingHuo <hym811@outlook.com>
2023-03-26 15:36:54 +0100

    Complete the reverse proxy

--- news/src/App.js
+++ news/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import Child from './Child';
 import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function app() {
+
+    useEffect(() => {
+        axios.get('/ajax/movieOnInfoList?token=&optimus_uuid=74B5F0A032A711EB82DD6B9282E93C676D27D7B9731D4E608D7612C3E708C120&optimus_risk_level=71&optimus_code=10').then(res => { console.log(res.data) }
+        )
+    }, [])
+
+    return (
+        <div>
+            app
+            <ul>
+                <li>item 1</li>
+                <li>item 2</li>
+                <li>item 3</li>
+            </ul>
+            <Child />
+        </div>
+    );
 }
 
-export default App;
+export default app;
