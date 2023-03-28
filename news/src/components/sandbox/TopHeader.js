843110e8efd1158a255711952329a3537a6be975
YumingHuo <hym811@outlook.com>
2023-03-28 23:12:43 +0100

    159 finish layout

--- news/src/components/sandbox/TopHeader.js
+++ news/src/components/sandbox/TopHeader.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Layout } from 'antd';
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+} from '@ant-design/icons';
+const { Header } = Layout;
 
 export default function TopHeader() {
+    const [collapsed] = useState(false)
     return (
-        <div>TopHeader</div>
+        <Header className='site-layout-background' style={{ padding: 0}}>
+        {
+            collapsed?<MenuUnfoldOutlined/>:<MenuFoldOutlined/>
+        }
+        </Header>
     )
 }
--- news/src/views/sandbox/NewsSandBox.js
+++ news/src/views/sandbox/NewsSandBox.js
@@ -7,21 +7,36 @@ import Nopermission from './nopermission/Nopermission'
 import RightList from './right-manage/RightList'
 import RoleList from './right-manage/RoleList'
 import UserList from './user-manage/UserList'
+import './NewsSandBox.css'
 
+import { Layout } from 'antd'
+const { Content } = Layout
+  
 export default function NewsSandBox() {
   return (
-    <div>
+    <Layout>
       <SideMenu></SideMenu>
-      <TopHeader></TopHeader>
+      <Layout className="site-layout">
+        <TopHeader></TopHeader>
 
-      <Switch>
-        <Route path="/home" component={Home} />
-        <Route path="/user-manage/list" component={UserList} />
-        <Route path="/right-manage/role/list" component={RoleList} />
-        <Route path="/right-manage/right/list" component={RightList} />
-        <Redirect from='/' to='/home' exact />
-        <Route path={'*'} component={Nopermission} />
-      </Switch>
-    </div>
+        <Content
+          className='site-layout-background'
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+          }}
+        >
+          <Switch>
+            <Route path="/home" component={Home} />
+            <Route path="/user-manage/list" component={UserList} />
+            <Route path="/right-manage/role/list" component={RoleList} />
+            <Route path="/right-manage/right/list" component={RightList} />
+            <Redirect from='/' to='/home' exact />
+            <Route path={'*'} component={Nopermission} />
+          </Switch>
+        </Content>
+      </Layout>
+    </Layout>
   )
 }
