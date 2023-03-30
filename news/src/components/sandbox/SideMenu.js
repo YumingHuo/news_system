4377789cb789e46f7785c4ccf68ce479517b80ae
YumingHuo <hym811@outlook.com>
2023-03-30 21:44:53 +0100

    back end side menu

--- news/src/components/sandbox/SideMenu.js
+++ news/src/components/sandbox/SideMenu.js
@@ -1,62 +1,84 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import { Layout, Menu } from 'antd';
 import './index.css'
 import {withRouter} from 'react-router-dom'
 import {
   UserOutlined
 } from '@ant-design/icons';
+import axios from 'axios'
 const { Sider } = Layout;
 const { SubMenu } = Menu
 
 //模拟数组结构
-const  menuList = [
-  {
-    key:"/home",
-    title:"首页",
-    icon:<UserOutlined />
-  },
-  {
-    key:"/user-manage",
-    title:"用户管理",
-    icon:<UserOutlined />,
-    children:[
-      {
-        key:"/user-manage/list",
-        title:"用户列表",
-        icon:<UserOutlined />
-      }
-    ]
-  },
-  {
-    key:"/right-manage",
-    title:"权限管理",
-    icon:<UserOutlined />,
-    children:[
-      {
-        key:"/right-manage/role/list",
-        title:"角色列表",
-        icon:<UserOutlined />
-      },
-      {
-        key:"/right-manage/right/list",
-        title:"权限列表",
-        icon:<UserOutlined />
-      }
-    ]
-  }
-]
+// const  menuList = [
+//   {
+//     key:"/home",
+//     title:"首页",
+//     icon:<UserOutlined />
+//   },
+//   {
+//     key:"/user-manage",
+//     title:"用户管理",
+//     icon:<UserOutlined />,
+//     children:[
+//       {
+//         key:"/user-manage/list",
+//         title:"用户列表",
+//         icon:<UserOutlined />
+//       }
+//     ]
+//   },
+//   {
+//     key:"/right-manage",
+//     title:"权限管理",
+//     icon:<UserOutlined />,
+//     children:[
+//       {
+//         key:"/right-manage/role/list",
+//         title:"角色列表",
+//         icon:<UserOutlined />
+//       },
+//       {
+//         key:"/right-manage/right/list",
+//         title:"权限列表",
+//         icon:<UserOutlined />
+//       }
+//     ]
+//   }
+// ]
+const iconList = {
+  "/home":<UserOutlined />,
+  "/user-manage":<UserOutlined />,
+  "/user-manage/list":<UserOutlined />,
+  "/right-manage":<UserOutlined />,
+  "/right-manage/role/list":<UserOutlined />,
+  "/right-manage/right/list":<UserOutlined />
+  //.......
+}
+
 
 function SideMenu(props) {
+  const [meun, setMeun] = useState([])
+  useEffect(()=>{
+    axios.get("http://localhost:5000/rights?_embed=children").then(res=>{
+      console.log(res.data)
+      setMeun(res.data)
+    })
+  },[])
 
+
+  const checkPagePermission = (item)=>{
+    return item.pagepermisson
+  }
   const renderMenu = (menuList)=>{
     return menuList.map(item=>{
-      if(item.children){
-        return <SubMenu key={item.key} icon={item.icon} title={item.title}>
+      if(item.children && checkPagePermission(item)){
+        return <SubMenu key={item.key} icon={iconList[item.key]} title={item.title}>
            { renderMenu(item.children) }
         </SubMenu>
       }
 
-      return <Menu.Item key={item.key} icon={item.icon} onClick={()=>{
+      return checkPagePermission(item) && <Menu.Item key={item.key} icon={iconList[item.key]}  onClick={()=>{
         //  console.log(props)
         props.history.push(item.key)
       }}>{item.title}</Menu.Item>
@@ -66,7 +88,7 @@ function SideMenu(props) {
     <Sider trigger={null} collapsible collapsed={false}>
       <div className="logo" >全球新闻发布管理系统</div>
       <Menu theme="dark" mode="inline" defaultSelectedKeys={['3']}>
-          {renderMenu(menuList)}
+          {renderMenu(meun)}
       </Menu>
     </Sider>
   )
