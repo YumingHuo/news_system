2987330accd7f138e8eb7975ff230394080fbb3d
YumingHuo <hym811@outlook.com>
2023-03-30 21:45:23 +0100

    finish side menu

--- news/src/components/sandbox/SideMenu.js
+++ news/src/components/sandbox/SideMenu.js
@@ -72,7 +72,7 @@ function SideMenu(props) {
   }
   const renderMenu = (menuList)=>{
     return menuList.map(item=>{
-      if(item.children && checkPagePermission(item)){
+      if(item.children?.length>0 && checkPagePermission(item)){
         return <SubMenu key={item.key} icon={iconList[item.key]} title={item.title}>
            { renderMenu(item.children) }
         </SubMenu>
@@ -84,12 +84,20 @@ function SideMenu(props) {
       }}>{item.title}</Menu.Item>
     })
   }
+
+  // console.log(props.location.pathname)
+  const selectKeys = [props.location.pathname]
+  const openKeys = ["/"+props.location.pathname.split("/")[1]]
   return (
-    <Sider trigger={null} collapsible collapsed={false}>
-      <div className="logo" >全球新闻发布管理系统</div>
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['3']}>
-          {renderMenu(meun)}
-      </Menu>
+    <Sider trigger={null} collapsible collapsed={false} >
+      <div style={{display:"flex",height:"100%","flexDirection":"column"}}>
+        <div className="logo" >全球新闻发布管理系统</div>
+        <div style={{flex:1,"overflow":"auto"}}>
+          <Menu theme="dark" mode="inline" selectedKeys={selectKeys} className="aaaaaaa" defaultOpenKeys={openKeys}>
+              {renderMenu(meun)}
+          </Menu>
+        </div>
+      </div>
     </Sider>
   )
 }
