2d33e43016a03d69f0ba4702ef776623ac03112e
YumingHuo <hym811@outlook.com>
2023-03-31 10:23:51 +0100

    Permission List- Delete

--- news/src/views/sandbox/right-manage/RightList.js
+++ news/src/views/sandbox/right-manage/RightList.js
@@ -1,53 +1,90 @@
-import React,{useState,useEffect} from 'react'
-import {Button, Table,Tag} from 'antd'
+import React, { useState, useEffect } from 'react'
+import { Button, Table, Tag, Modal } from 'antd'
 import axios from 'axios'
-import {DeleteOutlined,EditOutlined} from '@ant-design/icons'
+import { DeleteOutlined, EditOutlined, ExclamationCircleOutlined } from '@ant-design/icons'
+const { confirm } = Modal
 export default function RightList() {
     const [dataSource, setdataSource] = useState([])
 
-    useEffect(()=>{
-        axios.get("http://localhost:5000/rights?_embed=children").then(res=>{
+    useEffect(() => {
+        axios.get("http://localhost:5000/rights?_embed=children").then(res => {
             const list = res.data
-            list[0].children = ""
+
+            list.forEach(item => {
+                if (item.children.length === 0) {
+                    item.children = ""
+                }
+            })
             setdataSource(list)
         })
-    },[])
+    }, [])
 
     const columns = [
         {
-          title: 'ID',
-          dataIndex: 'id',
-          render:(id)=>{
-          return <b>{id}</b>
-          }
+            title: 'ID',
+            dataIndex: 'id',
+            render: (id) => {
+                return <b>{id}</b>
+            }
         },
         {
             title: '权限名称',
             dataIndex: 'title'
         },
         {
-            title:"权限路径",
-            dataIndex:'key',
-            render:(key)=>{
-            return <Tag color="orange">{key}</Tag>
+            title: "权限路径",
+            dataIndex: 'key',
+            render: (key) => {
+                return <Tag color="orange">{key}</Tag>
             }
         },
         {
-            title:"操作",
-            render:()=>{
-            return <div>
-                <Button danger shape="circle" icon={<DeleteOutlined />} />
-                <Button type="primary" shape="circle" icon={<EditOutlined />} />
-            </div>
+            title: "操作",
+            render: (item) => {
+                return <div>
+                    <Button danger shape="circle" icon={<DeleteOutlined />} onClick={() => confirmMethod(item)} />
+                    <Button type="primary" shape="circle" icon={<EditOutlined />} />
+                </div>
             }
         }
-      ];
+    ];
+
+    const confirmMethod = (item) => {
+        confirm({
+            title: '你确定要删除?',
+            icon: <ExclamationCircleOutlined />,
+            // content: 'Some descriptions',
+            onOk() {
+                //   console.log('OK');
+                deleteMethod(item)
+            },
+            onCancel() {
+                //   console.log('Cancel');
+            },
+        });
+
+    }
+    //删除
+    const deleteMethod = (item) => {
+        // console.log(item)
+        // 当前页面同步状态 + 后端同步
+        if (item.grade === 1) {
+            setdataSource(dataSource.filter(data => data.id !== item.id))
+            axios.delete(`http://localhost:5000/rights/${item.id}`)
+        }else{
+            let list = dataSource.filter(data=>data.id===item.rightId)
+            list[0].children = list[0].children.filter(data=>data.id!==item.id)
+            setdataSource([...dataSource])
+            axios.delete(`http://localhost:5000/children/${item.id}`)
+        }
+    }
+
     return (
         <div>
-            <Table dataSource={dataSource} columns={columns} 
-            pagination={{
-                pageSize:5
-            }}/>
+            <Table dataSource={dataSource} columns={columns}
+                pagination={{
+                    pageSize: 5
+                }} />
         </div>
     )
 }
