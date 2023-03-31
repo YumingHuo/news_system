155b7e4ad881c4077c4a1c027c6fffa8aa54a2a5
YumingHuo <hym811@outlook.com>
2023-03-31 11:05:19 +0100

    Role List - Import

--- news/src/views/sandbox/user-manage/UserList.js
+++ news/src/views/sandbox/user-manage/UserList.js
@@ -1,9 +1,86 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import { Button, Table,Modal, Switch} from 'antd'
+import axios from 'axios'
+import { DeleteOutlined, EditOutlined, ExclamationCircleOutlined } from '@ant-design/icons'
+const { confirm } = Modal
 export default function UserList() {
+    const [dataSource, setdataSource] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:5000/users?_expand=role").then(res => {
+            const list = res.data
+            setdataSource(list)
+        })
+    }, [])
+
+    const columns = [
+        {
+            title: '区域',
+            dataIndex: 'region',
+            render: (region) => {
+                return <b>{region===""?'全球':region}</b>
+            }
+        },
+        {
+            title: '角色名称',
+            dataIndex: 'role',
+            render:(role)=>{
+                return role?.roleName
+            }
+        },
+        {
+            title: "用户名",
+            dataIndex: 'username'
+        },
+        {
+            title: "用户状态",
+            dataIndex: 'roleState',
+            render:(roleState,item)=>{
+                return <Switch checked={roleState}  disabled={item.default}></Switch>
+            }
+        },
+        {
+            title: "操作",
+            render: (item) => {
+                return <div>
+                    <Button danger shape="circle" icon={<DeleteOutlined />} onClick={() => confirmMethod(item)} disabled={item.default}/>
+                    
+                    <Button type="primary" shape="circle" icon={<EditOutlined />} disabled={item.default}/>
+                </div>
+            }
+        }
+    ];
+
+    const confirmMethod = (item) => {
+        confirm({
+            title: '你确定要删除?',
+            icon: <ExclamationCircleOutlined />,
+            // content: 'Some descriptions',
+            onOk() {
+                //   console.log('OK');
+                deleteMethod(item)
+            },
+            onCancel() {
+                //   console.log('Cancel');
+            },
+        });
+
+    }
+    //删除
+    const deleteMethod = (item) => {
+        // console.log(item)
+        // 当前页面同步状态 + 后端同步
+       
+    }
+
     return (
         <div>
-            userlist
+            <Table dataSource={dataSource} columns={columns}
+                pagination={{
+                    pageSize: 5
+                }} 
+                rowKey={item=>item.id}
+                />
         </div>
     )
 }
