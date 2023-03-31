779786a51fa6661a7856de775a98b524f659d369
YumingHuo <hym811@outlook.com>
2023-03-31 10:22:55 +0100

    tree list

--- news/src/views/sandbox/right-manage/RightList.js
+++ news/src/views/sandbox/right-manage/RightList.js
@@ -1,19 +1,25 @@
 import React,{useState,useEffect} from 'react'
-import {Table} from 'antd'
+import {Button, Table,Tag} from 'antd'
 import axios from 'axios'
+import {DeleteOutlined,EditOutlined} from '@ant-design/icons'
 export default function RightList() {
     const [dataSource, setdataSource] = useState([])
 
     useEffect(()=>{
-        axios.get("http://localhost:5000/rights").then(res=>{
-            setdataSource(res.data)
+        axios.get("http://localhost:5000/rights?_embed=children").then(res=>{
+            const list = res.data
+            list[0].children = ""
+            setdataSource(list)
         })
     },[])
 
     const columns = [
         {
           title: 'ID',
-          dataIndex: 'id'
+          dataIndex: 'id',
+          render:(id)=>{
+          return <b>{id}</b>
+          }
         },
         {
             title: '权限名称',
@@ -21,12 +27,27 @@ export default function RightList() {
         },
         {
             title:"权限路径",
-            dataIndex:'key'
+            dataIndex:'key',
+            render:(key)=>{
+            return <Tag color="orange">{key}</Tag>
+            }
+        },
+        {
+            title:"操作",
+            render:()=>{
+            return <div>
+                <Button danger shape="circle" icon={<DeleteOutlined />} />
+                <Button type="primary" shape="circle" icon={<EditOutlined />} />
+            </div>
+            }
         }
       ];
     return (
         <div>
-            <Table dataSource={dataSource} columns={columns} />
+            <Table dataSource={dataSource} columns={columns} 
+            pagination={{
+                pageSize:5
+            }}/>
         </div>
     )
 }
