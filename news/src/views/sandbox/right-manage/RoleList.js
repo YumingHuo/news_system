63f7f77cfdb26e1650588109549f150d2f47cea2
YumingHuo <hym811@outlook.com>
2023-03-31 10:49:56 +0100

    Role List - Import

--- news/src/views/sandbox/right-manage/RoleList.js
+++ news/src/views/sandbox/right-manage/RoleList.js
@@ -1,9 +1,65 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import { Table,Button,Modal } from 'antd'
+import axios from 'axios'
+import { DeleteOutlined, EditOutlined, ExclamationCircleOutlined } from '@ant-design/icons'
+const {confirm} = Modal
 export default function RoleList() {
+    const [dataSource, setdataSource] = useState([])
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            render: (id) => {
+                return <b>{id}</b>
+            }
+        },
+        {
+            title: '角色名称',
+            dataIndex: 'roleName'
+        },
+        {
+            title: "操作",
+            render: (item) => {
+                return <div>
+                    <Button danger shape="circle" icon={<DeleteOutlined />} onClick={() => confirmMethod(item)} />
+                    <Button type="primary" shape="circle" icon={<EditOutlined />} />
+                </div>
+            }
+        }
+    ]
+
+    const confirmMethod = (item) => {
+        confirm({
+            title: '你确定要删除?',
+            icon: <ExclamationCircleOutlined />,
+            // content: 'Some descriptions',
+            onOk() {
+                //   console.log('OK');
+                deleteMethod(item)
+            },
+            onCancel() {
+                //   console.log('Cancel');
+            },
+        });
+
+    }
+    //删除
+    const deleteMethod = (item) => {
+        // console.log(item)
+        setdataSource(dataSource.filter(data => data.id !== item.id))
+        axios.delete(`http://localhost:5000/roles/${item.id}`)
+    }
+
+    useEffect(() => {
+        axios.get("http://localhost:5000/roles").then(res => {
+            // console.log(res.data)
+            setdataSource(res.data)
+        })
+    }, [])
     return (
         <div>
-            RoleList
+            <Table dataSource={dataSource} columns={columns}
+                rowKey={(item) => item.id}></Table>
         </div>
     )
 }
