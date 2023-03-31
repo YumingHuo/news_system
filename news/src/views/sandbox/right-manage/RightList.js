b0bcae10b88c08f4ad2a8bd636b842eef8a1abd2
YumingHuo <hym811@outlook.com>
2023-03-31 10:15:20 +0100

    Import permission list

--- news/src/views/sandbox/right-manage/RightList.js
+++ news/src/views/sandbox/right-manage/RightList.js
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React,{useState,useEffect} from 'react'
+import {Table} from 'antd'
+import axios from 'axios'
 export default function RightList() {
+    const [dataSource, setdataSource] = useState([])
+
+    useEffect(()=>{
+        axios.get("http://localhost:5000/rights").then(res=>{
+            setdataSource(res.data)
+        })
+    },[])
+
+    const columns = [
+        {
+          title: 'ID',
+          dataIndex: 'id'
+        },
+        {
+            title: '权限名称',
+            dataIndex: 'title'
+        },
+        {
+            title:"权限路径",
+            dataIndex:'key'
+        }
+      ];
     return (
         <div>
-            RightList
+            <Table dataSource={dataSource} columns={columns} />
         </div>
     )
 }
