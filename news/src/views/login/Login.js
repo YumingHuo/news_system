13f7fa3ae057efc09a8308183d423c3a3d463430
YumingHuo <hym811@outlook.com>
2023-04-01 11:02:57 +0100

    Landing Page - Particle Effect

--- news/src/views/login/Login.js
+++ news/src/views/login/Login.js
@@ -2,14 +2,170 @@ import React from 'react'
 import { Form, Button, Input } from 'antd'
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import './Login.css'
+import Particles from 'react-particles-js';
+
 export default function Login() {
 
     const onFinish = (values) => {
         console.log(values)
     }
     return (
-        <div style={{ background: 'rgb(35, 39, 65)', height: "100%" }}>
-
+        <div style={{ background: 'rgb(35, 39, 65)', height: "100%",overflow:'hidden' }}>
+            <Particles height={document.documentElement.clientHeight}params={
+                {
+                    "background": {
+                      "color": {
+                        "value": "rgb(35, 39, 65)"
+                      },
+                      "position": "50% 50%",
+                      "repeat": "no-repeat",
+                      "size": "cover"
+                    },
+                    "fullScreen": {
+                      "enable": true,
+                      "zIndex": 1
+                    },
+                    "interactivity": {
+                      "events": {
+                        "onClick": {
+                          "enable": true,
+                          "mode": "push"
+                        },
+                        "onHover": {
+                          "enable": true,
+                          "mode": "bubble",
+                          "parallax": {
+                            "force": 60
+                          }
+                        }
+                      },
+                      "modes": {
+                        "bubble": {
+                          "distance": 400,
+                          "duration": 2,
+                          "opacity": 1,
+                          "size": 40
+                        },
+                        "grab": {
+                          "distance": 400
+                        }
+                      }
+                    },
+                    "particles": {
+                      "color": {
+                        "value": "#ffffff"
+                      },
+                      "links": {
+                        "color": {
+                          "value": "#fff"
+                        },
+                        "distance": 150,
+                        "opacity": 0.4
+                      },
+                      "move": {
+                        "attract": {
+                          "rotate": {
+                            "x": 600,
+                            "y": 1200
+                          }
+                        },
+                        "enable": true,
+                        "outModes": {
+                          "default": "bounce",
+                          "bottom": "bounce",
+                          "left": "bounce",
+                          "right": "bounce",
+                          "top": "bounce"
+                        },
+                        "speed": 6
+                      },
+                      "number": {
+                        "density": {
+                          "enable": true
+                        },
+                        "value": 170
+                      },
+                      "opacity": {
+                        "animation": {
+                          "speed": 1,
+                          "minimumValue": 0.1
+                        }
+                      },
+                      "shape": {
+                        "options": {
+                          "character": {
+                            "fill": false,
+                            "font": "Verdana",
+                            "style": "",
+                            "value": "*",
+                            "weight": "400"
+                          },
+                          "char": {
+                            "fill": false,
+                            "font": "Verdana",
+                            "style": "",
+                            "value": "*",
+                            "weight": "400"
+                          },
+                          "polygon": {
+                            "nb_sides": 5
+                          },
+                          "star": {
+                            "nb_sides": 5
+                          },
+                          "image": {
+                            "height": 32,
+                            "replace_color": true,
+                            "src": "/logo192.png",
+                            "width": 32
+                          },
+                          "images": {
+                            "height": 32,
+                            "replace_color": true,
+                            "src": "/logo192.png",
+                            "width": 32
+                          }
+                        },
+                        "type": "image"
+                      },
+                      "size": {
+                        "value": 16,
+                        "animation": {
+                          "speed": 40,
+                          "minimumValue": 0.1
+                        }
+                      },
+                      "stroke": {
+                        "color": {
+                          "value": "#000000",
+                          "animation": {
+                            "h": {
+                              "count": 0,
+                              "enable": false,
+                              "offset": 0,
+                              "speed": 1,
+                              "sync": true
+                            },
+                            "s": {
+                              "count": 0,
+                              "enable": false,
+                              "offset": 0,
+                              "speed": 1,
+                              "sync": true
+                            },
+                            "l": {
+                              "count": 0,
+                              "enable": false,
+                              "offset": 0,
+                              "speed": 1,
+                              "sync": true
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+            }/>
             <div className="formContainer">
                 <div className="logintitle">全球新闻发布管理系统</div>
                 <Form
