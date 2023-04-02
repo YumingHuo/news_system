4a00019459f3285032b6816913edc5d5564435b9
YumingHuo <hym811@outlook.com>
2023-04-02 11:02:14 +0100

    Write a News - Form

--- news/src/views/sandbox/news-manage/NewsAdd.js
+++ news/src/views/sandbox/news-manage/NewsAdd.js
@@ -1,17 +1,44 @@
-import React,{useState} from 'react'
-import { PageHeader ,Steps,Button} from 'antd'
+import React, { useEffect, useState,useRef } from 'react'
+import { PageHeader, Steps, Button, Form, Input, Select } from 'antd'
 import style from './News.module.css'
+import axios from 'axios'
 const { Step } = Steps;
+const {Option} = Select;
 
 export default function NewsAdd() {
     const [current, setCurrent] = useState(0)
+    const [categoryList, setCategoryList] = useState([])
 
-    const handleNext = ()=>{
-        setCurrent(current+1)
+    const handleNext = () => {
+        if(current===0){
+            NewsForm.current.validateFields().then(res=>{
+                console.log(res)
+                setCurrent(current + 1)
+            }).catch(error=>{
+                console.log(error)
+            })
+        }else{
+            setCurrent(current + 1)
+        }
     }
-    const handlePrevious = ()=>{
-        setCurrent(current-1)
+    const handlePrevious = () => {
+        setCurrent(current - 1)
     }
+
+    const layout = {
+        labelCol: { span: 4 },
+        wrapperCol: { span: 20 },
+    }
+
+    const NewsForm = useRef(null)
+
+    useEffect(()=>{
+        axios.get("/categories").then(res=>{
+            // console.log(res.data)
+            setCategoryList(res.data)
+        })
+    },[])
+
     return (
         <div>
             <PageHeader
@@ -25,27 +52,57 @@ export default function NewsAdd() {
                 <Step title="新闻内容" description="新闻主体内容" />
                 <Step title="新闻提交" description="保存草稿或者提交审核" />
             </Steps>
-            
-           
-            <div className={current===0?'':style.active}>1111111
-                <input type="text"/>
-            </div>
 
-            <div className={current===1?'':style.active}>22222</div>
-            <div className={current===2?'':style.active}>3333</div>
 
             <div style={{marginTop:"50px"}}>
+                <div className={current === 0 ? '' : style.active}>
+
+                    <Form
+                        {...layout}
+                        name="basic"
+                        ref={NewsForm}
+                    >
+                        <Form.Item
+                            label="新闻标题"
+                            name="title"
+                            rules={[{ required: true, message: 'Please input your username!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="新闻分类"
+                            name="categoryId"
+                            rules={[{ required: true, message: 'Please input your username!' }]}
+                        >
+                            <Select>
+                                {
+                                    categoryList.map(item=>
+                                    <Option value={item.id} key={item.id}>{item.title}</Option>    
+                                    )
+                                }
+                            </Select>
+                        </Form.Item>
+
+                    </Form>
+                </div>
+
+                <div className={current === 1 ? '' : style.active}>22222</div>
+                <div className={current === 2 ? '' : style.active}>3333</div>
+
+            </div>
+            <div style={{ marginTop: "50px" }}>
                 {
-                    current===2 && <span>
+                    current === 2 && <span>
                         <Button type="primary">保存草稿箱</Button>
                         <Button danger>提交审核</Button>
                     </span>
                 }
                 {
-                    current<2 && <Button type="primary" onClick={handleNext}>下一步</Button>
+                    current < 2 && <Button type="primary" onClick={handleNext}>下一步</Button>
                 }
                 {
-                    current>0 && <Button onClick={handlePrevious}>上一步</Button>
+                    current > 0 && <Button onClick={handlePrevious}>上一步</Button>
                 }
             </div>
         </div>
