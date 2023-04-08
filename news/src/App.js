e48c804eb26abc95e22e09d4246fc3ae863272a2
YumingHuo <hym811@outlook.com>
2023-04-08 15:34:56 +0100

    State Management - Persistence

--- news/src/App.js
+++ news/src/App.js
@@ -1,10 +1,14 @@
 import IndexRouter from './router/IndexRouter'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import './App.css'
-import store from './redux/store'
-function App(){
-  return <Provider store={store}> 
-    <IndexRouter></IndexRouter>
+import { store,persistor } from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+
+function App() {
+  return <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <IndexRouter></IndexRouter>
+    </PersistGate>
   </Provider>
 }
 export default App
--- news/src/redux/store.js
+++ news/src/redux/store.js
@@ -1,13 +1,29 @@
 import {createStore,combineReducers} from 'redux'
 import {CollApsedReducer} from './reducers/CollapsedReducer'
 import {LoadingReducer} from './reducers/LoadingReducer'
+
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+
+const persistConfig = {
+    key: 'kerwin',
+    storage,
+    blacklist: ['LoadingReducer'] 
+}
 const reducer = combineReducers({
     CollApsedReducer,
     LoadingReducer
 })
-const store = createStore(reducer);
+const persistedReducer = persistReducer(persistConfig, reducer)
+  
 
-export default store
+const store = createStore(persistedReducer);
+const persistor = persistStore(store)
+export {
+    store,
+    persistor
+}
 
 /*
  store.dispatch()
