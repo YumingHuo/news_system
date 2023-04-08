8883e5c630a75687fd5488029fd7e61e9df212d7
YumingHuo <hym811@outlook.com>
2023-04-08 15:30:34 +0100

    Release Management - Component Packaging

--- news/src/views/sandbox/publish-manage/Unpublished.js
+++ news/src/views/sandbox/publish-manage/Unpublished.js
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React,{useEffect, useState} from 'react'
+import axios from 'axios'
+import NewsPublish from '../../../components/publish-manage/NewsPublish'
 export default function Unpublished() {
+
+    const {username} = JSON.parse(localStorage.getItem("token"))
+
+    const [dataSource, setdataSource] = useState([])
+    useEffect(() => {
+
+        axios(`/news?author=${username}&publishState=1&_expand=category`).then(res=>{
+            // console.log(res.data)
+            setdataSource(res.data)
+        })
+    }, [username])
+
     return (
         <div>
-            Unpublished
+            <NewsPublish dataSource={dataSource}></NewsPublish>
         </div>
     )
 }
