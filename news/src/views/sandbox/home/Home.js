09937353e535c656bdc97e2de3cb773994965dcd
YumingHuo <hym811@outlook.com>
2023-04-09 15:26:12 +0100

    Homepage data display-typesetting

--- news/src/views/sandbox/home/Home.js
+++ news/src/views/sandbox/home/Home.js
@@ -1,47 +1,56 @@
 import React from 'react'
-import { Button } from 'antd';
+import { Card, Col, Row, List,Avatar } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+const { Meta } = Card;
+
 // import axios from 'axios'
 export default function Home() {
 
-    const ajax = ()=>{
-        //取数据  get
-        // axios.get("http://localhost:8000/posts/2").then(res=>{
-        //     console.log(res.data)
-        // })
-
-        // 增  post
-        // axios.post("http://localhost:8000/posts",{
-        //     title:"33333",
-        //     author:"xiaoming"
-        // })
-
-        // 更新 put
-
-        // axios.put("http://localhost:8000/posts/1",{
-        //     title:"1111-修改"
-        // })
-
-        // 更新 patch
-        // axios.patch("http://localhost:8000/posts/1",{
-        //     title:"1111-修改-11111"
-        // }) 
-
-        // 删除  delete
-        // axios.delete("http://localhost:8000/posts/1")
-    
-        // _embed
-        // axios.get("http://localhost:8000/posts?_embed=comments").then(res=>{
-        //     console.log(res.data)
-        // })
-
-        // _expand
-        // axios.get("http://localhost:8000/comments?_expand=post").then(res=>{
-        //     console.log(res.data)
-        // })
-    }
     return (
-        <div>
-            <Button type="primary" onClick={ajax}>Button</Button>
+        <div className="site-card-wrapper">
+            <Row gutter={16}>
+                <Col span={8}>
+                    <Card title="用户最常浏览" bordered={true}>
+                        <List
+                            size="small"
+                            // bordered
+                            dataSource={["1111", "222", "33333"]}
+                            renderItem={item => <List.Item>{item}</List.Item>}
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card title="用户点赞最多" bordered={true}>
+                        <List
+                            size="small"
+                            // bordered
+                            dataSource={["1111", "222", "33333"]}
+                            renderItem={item => <List.Item>{item}</List.Item>}
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card
+                        cover={
+                            <img
+                                alt="example"
+                                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                            />
+                        }
+                        actions={[
+                            <SettingOutlined key="setting" />,
+                            <EditOutlined key="edit" />,
+                            <EllipsisOutlined key="ellipsis" />,
+                        ]}
+                    >
+                        <Meta
+                            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                            title="Card title"
+                            description="This is the description"
+                        />
+                    </Card>
+                </Col>
+            </Row>
         </div>
     )
 }
